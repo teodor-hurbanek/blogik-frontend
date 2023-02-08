bc89f001188954925d4ba4a444d294a11a615d4c
Teodor Hurbanek <teodorhurbanek@gmail.com>
2023-02-08 11:10:38 +0100

    starting commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,24 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
+
+const theme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+  typography: {
+    button: {
+      textTransform: 'none',
+    },
+  },
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
---
+++ src/pages/contact.tsx
@@ -0,0 +1,9 @@
+import Layout from '@/components/Layout'
+
+export default function Contact() {
+  return (
+    <Layout>
+      <div>Contact</div>
+    </Layout>
+  )
+}
